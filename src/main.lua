6496ef1ba51b0c18b027d7f4b2f6eb929a5e7ab1
mramiro <maxime.ramiro@selinko.com>
2017-06-21 00:58:21 +0200

    fix ressource printing order

--- src/main.lua
+++ src/main.lua
@@ -38,7 +38,7 @@ function print_log()
   print('ram: '..(ram or 0)..'/1024', cam.x+1, cam.y+8, 0)
   print('entities: '..count(entities), cam.x+1, cam.y+14, 0)
   print(is_between_two_walls(flr(mouse.x/8), flr(mouse.y/8)), cam.x+1, cam.y+20, 0)
-  print('counter: '..counter, cam.x+1, cam.y+26)
-
+  
+  --print('counter: '..counter, cam.x+1, cam.y+26)
   -- flr(mouse.x/8), flr(mouse.y/8)
 end
--- src/o_ressources.lua
+++ src/o_ressources.lua
@@ -3,11 +3,11 @@ function ressources:new(o)
   r = o or {}
   r.wood = 0
   r.stone = 0
-  r.people = 0
+  r.food = 0
 
   r.building_wood = 1
   r.building_stone = 2
-  r.building_people = 3
+  r.building_food = 3
 
   setmetatable(o, self)
   self.__index = self
@@ -24,9 +24,9 @@ function ressources:show()
   local x = flr(cam.x/8)*8 + 13*8 + 1
   local y = flr(cam.y/8)*8 + 1
 
-  print(self.wood, x, y + 13*8, 7)
+  print(self.food,  x, y + 13*8, 7)
   print(self.stone, x, y + 14*8, 7)
-  print(self.people, x, y + 15*8, 7)
+  print(self.wood,  x, y + 15*8, 7)
 end
 
 function ressources:update()
@@ -34,7 +34,7 @@ function ressources:update()
   if ((counter % refresh_value) == 0) then
     self.wood += self.building_wood
     self.stone += self.building_stone
-    self.people += self.building_people
+    self.food += self.building_food
   end
 end
 
